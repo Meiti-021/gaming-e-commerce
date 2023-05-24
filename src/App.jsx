dfd69258c12f8ae606cd058cd9e2f354a75ec852
Mahdi <meiti.edu@gmail.com>
2023-05-25 00:34:10 +0430

    create header & side navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
