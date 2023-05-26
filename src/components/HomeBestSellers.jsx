4772991133455c592f97f64a9f11b6ffb510e987
Mahdi <meiti.edu@gmail.com>
2023-05-26 16:36:19 +0430

    create best seller swiper placeholder

--- src/components/HomeBestSellers.jsx
+++ src/components/HomeBestSellers.jsx
@@ -1,5 +1,11 @@
 import circle6 from "../assets/images/design-circle-3.png";
 import SectionHeader from "../components/SectionHeader";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper";
+
 const HomeBestSellers = () => {
   return (
     <div className="relative bg-black w-full h-auto mt-[27rem] xs:mt-[31rem] md:mt-[35rem] lg:mt-[36rem]">
@@ -8,8 +14,34 @@ const HomeBestSellers = () => {
         alt=""
         className="absolute -top-32 z-0 right-0 left-0 mx-auto"
       />
-      <div className="flex-col relative p-5 max-w-7xl w-full mx-auto z-50">
+      <div className="flex-col relative p-5 max-w-7xl w-full mx-auto z-40">
         <SectionHeader title="Bestsellers of the week" address="" />
+        <div className="">
+          <Swiper
+            slidesPerView={"auto"}
+            pagination={{
+              clickable: true,
+
+              type: "bullets",
+            }}
+            loop={true}
+            modules={[Pagination]}
+            className="mySwiper border-2 border-white h-[30rem] text-white"
+          >
+            <SwiperSlide className="w-40">Slide 1</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 2</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 3</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 4</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 1</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 2</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 3</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 4</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 1</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 2</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 3</SwiperSlide>
+            <SwiperSlide className="w-40">Slide 4</SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </div>
   );
