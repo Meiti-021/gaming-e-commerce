a2ed2530f619a37701892a0ac1086bd8001db715
Mahdi <meiti.edu@gmail.com>
2023-06-15 05:57:17 +0430

    fix flash of unstyled text (FOUT) issue

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Collections from "./pages/Collections";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Faqs from "./pages/Faqs";
@@ -21,11 +21,32 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import ScrollToTop from "./components/ScrollToTop";
 import WishList from "./pages/WishList";
-
+import "./assets/fonts/Outfit.ttf";
+import FontFaceObserver from "fontfaceobserver";
+import Loading from "./assets/images/loading.gif";
 function App() {
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     window.scrollTo(0, 0);
+    const font = new FontFaceObserver("Outfit");
+
+    font
+      .load()
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Font loading error:", error);
+      });
   }, []);
+
+  if (loading)
+    return (
+      <div className="flex h-screen w-full items-center justify-center bg-black">
+        <img src={loading} alt="" className="block h-48 object-contain" />
+      </div>
+    );
+
   return (
     <>
       <Router>
